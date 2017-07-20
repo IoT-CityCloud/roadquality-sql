060b950fc118a1d6759848dd16ef94aec8756ba6
Johan Hidding <johannes.hidding@gmail.com>
2017-07-20 16:14:05 +0200

    fix spell error in schema, update python script to insert csv data into db

--- roads-sql.py
+++ roads-sql.py
@@ -1,6 +1,7 @@
 import MySQLdb
 import numpy as np
 import os
+import getpass
 
 
 path = './data'
@@ -22,7 +23,44 @@ data = {
 
 
 for k, v in data.items():
-    print(k)
-    table = np.genfromtxt(os.path.join(path, v['filename']), dtype=v['format'], delimiter=',')
-    print(table)
+    print(k, " ... ")
+    v['table'] = np.genfromtxt(os.path.join(path, v['filename']), dtype=v['format'], delimiter=',')
+    # print(v['table'])
 
+pw = getpass.getpass()
+db = MySQLdb.connect(host='localhost', user='root', passwd=pw, db='roadquality')
+c = db.cursor()
+
+#### doing this messes things up...
+# schema = open('roads-schema.sql').read()
+# c.execute(schema)
+# c.close()
+# db.commit()
+
+c = db.cursor()
+c.execute("SELECT * FROM `Devices` WHERE `idDevices` = 0")
+result = c.fetchone()
+print("Device 0: {}".format(result))
+
+if result is None:
+    print("adding device")
+    c.execute("INSERT INTO `Devices` (idDevices, name) VALUES (%s, %s)", (0, "magic device #1",))
+
+c.executemany("INSERT INTO `GPS` (deviceId, time, accuracy, lattitude, longitude, altitude, bearing, speed)"
+              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+              [(0,) + tuple(v) for v in data['gps']['table']])
+
+c.executemany("INSERT INTO `Accelerometer` (deviceId, time, accuracy, x, y, z)"
+              "VALUES (%s, %s, %s, %s, %s, %s)",
+              [(0,) + tuple(v) for v in data['acceleration']['table']])
+
+c.executemany("INSERT INTO `LinearAcceleration` (deviceId, time, accuracy, x, y, z)"
+              "VALUES (%s, %s, %s, %s, %s, %s)",
+              [(0,) + tuple(v) for v in data['linear_accelerometer']['table']])
+
+c.executemany("INSERT INTO `Gyroscope` (deviceId, time, accuracy, x, y, z)"
+              "VALUES (%s, %s, %s, %s, %s, %s)",
+              [(0,) + tuple(v) for v in data['gyroscope']['table']])
+
+db.commit()
+#c.executemany("INSERT INTO `GPS`
